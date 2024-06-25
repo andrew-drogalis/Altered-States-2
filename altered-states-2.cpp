df9d287e5d9868b6300e489d198374d399b606f7
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-25 14:57:31 -0400

    Added constexpr state populations

---
+++ altered-states-2.cpp
@@ -0,0 +1,97 @@
+// Copyright 2024 Andrew Drogalis
+// GNU License
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string_view>
+
+constexpr int matrixSize {5};
+constexpr int numStates {50};
+
+using namespace std::literals::string_view_literals;
+// Compile time array with states and population as of 2020 census
+constexpr std::array<std::pair<std::string_view, int>, numStates> statesPopulation {{
+    {"alabama"sv, 5'024'279},
+    {"alaska"sv, 733'391},
+    {"arizona"sv, 7'151'502},
+    {"arkansas"sv, 3'011'524},
+    {"california"sv, 39'538'223},
+    {"colorado"sv, 5'773'714},
+    {"connecticut"sv, 3'605'944},
+    {"delaware"sv, 989'948},
+    {"florida"sv, 21'538'187},
+    {"georgia"sv, 10'711'908},
+    {"hawaii"sv, 1'455'271},
+    {"idaho"sv, 1'839'106},
+    {"illinois"sv, 12'812'508},
+    {"indiana"sv, 6'785'528},
+    {"iowa"sv, 3'190'369},
+    {"kansas"sv, 2'937'880},
+    {"kentucky"sv, 4'505'836},
+    {"louisiana"sv, 4'657'757},
+    {"maine"sv, 1'362'359},
+    {"maryland"sv, 6'177'224},
+    {"massachusetts"sv, 7'029'917},
+    {"michigan"sv, 10'077'331},
+    {"minnesota"sv, 5'706'494},
+    {"mississippi"sv, 2'961'279},
+    {"missouri"sv, 6'154'913},
+    {"montana"sv, 1'084'225},
+    {"nebraska"sv, 1'961'504},
+    {"nevada"sv, 3'104'614},
+    {"newhampshire"sv, 1'377'529},
+    {"newjersey"sv, 9'288'994},
+    {"newmexico"sv, 2'117'522},
+    {"newyork"sv, 20'201'249},
+    {"northcarolina"sv, 10'439'388},
+    {"northdakota"sv, 779'094},
+    {"ohio"sv, 11'799'448},
+    {"oklahoma"sv, 3'959'353},
+    {"oregon"sv, 4'237'256},
+    {"pennsylvania"sv, 13'002'700},
+    {"rhodeisland"sv, 1'097'379},
+    {"southcarolina"sv, 5'118'425},
+    {"southdakota"sv, 886'667},
+    {"tennessee"sv, 6'910'840},
+    {"texas"sv, 29'145'505},
+    {"utah"sv, 3'271'616},
+    {"vermont"sv, 643'077},
+    {"virginia"sv, 8'631'393},
+    {"washington"sv, 7'705'281},
+    {"westvirginia"sv, 1'793'716},
+    {"wisconsin"sv, 5'893'718},
+    {"wyoming"sv, 576'851},
+}};
+
+constexpr std::array<double, numStates> peoplePerLetter()
+{
+    int i {};
+    std::array<double, numStates> peoplePerLetterArr {};
+    for (auto [state, population] : statesPopulation)
+    {
+        peoplePerLetterArr[i] = population / static_cast<double>(state.length());
+        ++i;
+    }
+    return peoplePerLetterArr;
+}
+
+
+int main() { 
+    auto people = peoplePerLetter();
+    //
+    // auto resultsMatrix;
+
+    std::string resultsMatrixStr;
+    for (auto row : resultsMatrix) {
+        for (auto col : row) {
+            resultsMatrixStr.push_back(col);
+        }
+    }
+    // Save results to file for export to Jane Street for judging
+    std::ofstream saveMatrix {"json/result-matrix.json"};
+    if (saveMatrix.good())
+    {
+        saveMatrix.write(resultsMatrixStr);
+    }
+    return 0;
+}
--- altered-states.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Andrew Drogalis 
-// GNU License
-#include <iostream>
-
-int main()
-{
-return 0;
-}
