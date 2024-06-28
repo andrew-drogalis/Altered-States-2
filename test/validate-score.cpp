f3fd2e2b2d6ff5a2c6617696dbd3d0c32e865e73
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-27 21:08:01 -0400

    Testing Setup

--- test/validate-score.cpp
+++ test/validate-score.cpp
@@ -2,9 +2,12 @@
 // GNU License
 #include <cmath>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
+#include <cassert>
 
 #include "state-populations.h"
 // inline constexpr int numStates defined in "state-populations"
@@ -13,12 +16,49 @@
 
 constexpr int minThreshold {165'379'868};
 
-int calculateScore() {}
+std::unordered_map<std::string, double> statesPopulationHashMap;
+
+void setStatesHashMap()
+{
+    for (auto [state, population] : statesPopulation) { statesPopulationHashMap[std::string {state}] = population; }
+}
+
+int calculateScore(int matrixSize, std::string const& resultsMatrixStr)
+{
+    int totalScore {};
+    int const numOfDirections {8};
+    std::array<int, numOfDirections + 1> directions = {1, 0, -1, 0, 1, 1, -1, -1, 1};
+
+    std::function<void(int, int, int, int)> searchMatrix = [&](int index, int row, int col, int iter) {
+        for (int i {}; i < numOfDirections; ++i)
+        {
+            int nextRow = row + directions[i];
+            int nextCol = col + directions[i + 1];
+             std::cout << nextRow << nextCol << iter << '\n';
+            if (nextRow >= 0 && nextCol >= 0 && nextRow < matrixSize && nextCol < matrixSize)
+            {
+                int index = matrixSize * nextRow + nextCol;
+                searchMatrix(index, nextRow, nextCol, iter + 1);
+            }
+        }
+    };
+
+    for (int row {}; row < matrixSize; ++row)
+    {
+        for (int col {}; col < matrixSize; ++col)
+        {
+            int index = matrixSize * row + col;
+            std::cout << row << col << index << '\n';
+            searchMatrix(index, row, col, 1);
+        }
+    }
+    return totalScore;
+}
 
 std::string loadJSON()
 {
     std::string resultsMatrixStr;
-    std::ifstream loadMatrix {"json/result-matrix.json"};
+    std::ifstream loadMatrix {"../json/result-matrix.txt"};
     if (loadMatrix.is_open())
     {
         loadMatrix >> resultsMatrixStr;
@@ -38,16 +78,31 @@ std::string loadJSON()
 
 int main()
 {
-    std::vector<char> results;
-    std::ifstream loadMatrix {"json/"};
+    std::string resultsMatrixStr = loadJSON();
+    int const matrixSize         = std::sqrt(resultsMatrixStr.length());
+    assert(resultsMatrixStr.length() == matrixSize * matrixSize);
+    setStatesHashMap();
 
-    int const matrixSize = std::sqrt(results.size());
-
-    int totalScore = calculateScore();
+    int totalScore = calculateScore(matrixSize, resultsMatrixStr);
 
     if (totalScore >= minThreshold)
     {
-        std::cout << "Congratuations! Your Score of " << totalScore << " Passed!\n";
+        std::cout << "Congratulations! Your Score of " << totalScore << " Passed!\n";
+        if (totalScore >= 200'000'000)
+        {
+            std::cout << "Awarded 200 Million Achievement!";
+        }
+        if (statesPopulationHashMap["california"])
+        {
+            std::cout << "Awarded 'No California' Achievement!";
+        }
+        if (! statesPopulationHashMap["pennsylvania"])
+        {
+            std::cout << "Awarded 'Visited Pennsylvania' Achievement!";
+        }
+        // - 20S: visits at least 20 states
+        // - M8: contains all 8 states that begin with an M
+        // - 4C: contains the “Four Corners” states
     }
     else
     {
