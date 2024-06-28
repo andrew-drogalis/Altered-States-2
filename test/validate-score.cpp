95e144297e87c7fa2e126d5adfd1f86738dbf1f0
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-27 23:59:16 -0400

    Completed Testing

--- test/validate-score.cpp
+++ test/validate-score.cpp
@@ -1,13 +1,12 @@
 // Copyright 2024 Andrew Drogalis
 // GNU License
+#include <cassert>
 #include <cmath>
 #include <fstream>
 #include <functional>
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include <vector>
-#include <cassert>
+#include <unordered_set>
 
 #include "state-populations.h"
 // inline constexpr int numStates defined in "state-populations"
@@ -15,41 +14,69 @@
 // "state-populations"
 
 constexpr int minThreshold {165'379'868};
-
-std::unordered_map<std::string, double> statesPopulationHashMap;
-
-void setStatesHashMap()
-{
-    for (auto [state, population] : statesPopulation) { statesPopulationHashMap[std::string {state}] = population; }
-}
+std::unordered_set<std::string_view> statesVisited;
 
 int calculateScore(int matrixSize, std::string const& resultsMatrixStr)
 {
     int totalScore {};
+    // All 8 Directions for Kings moves in chess
     int const numOfDirections {8};
     std::array<int, numOfDirections + 1> directions = {1, 0, -1, 0, 1, 1, -1, -1, 1};
 
-    std::function<void(int, int, int, int)> searchMatrix = [&](int index, int row, int col, int iter) {
-        for (int i {}; i < numOfDirections; ++i)
-        {
-            int nextRow = row + directions[i];
-            int nextCol = col + directions[i + 1];
-             std::cout << nextRow << nextCol << iter << '\n';
-            if (nextRow >= 0 && nextCol >= 0 && nextRow < matrixSize && nextCol < matrixSize)
+    // Recursive Matrix Navigation
+    std::function<void(int, int, int, int, std::array<int, numStates>)> searchMatrix =
+        [&](int index, int row, int col, int level, std::array<int, numStates> errorCharCount) {
+            int i {};
+            bool breakOut = true;
+            for (const auto& [state, population] : statesPopulation)
             {
-                int index = matrixSize * nextRow + nextCol;
-                searchMatrix(index, nextRow, nextCol, iter + 1);
+                int lenStateWord = state.length();
+                if (level < lenStateWord && state[level] != resultsMatrixStr[index])
+                {
+                    errorCharCount[i]++;
+                }
+                if (level >= lenStateWord)
+                {
+                    errorCharCount[i]++;
+                }
+                // Store the result
+                if (level == lenStateWord - 1 && errorCharCount[i] <= 1 && ! statesVisited.contains(state))
+                {
+                    totalScore += population;
+                    statesVisited.insert(state);
+                }
+                // If we still have a chance then continue the search
+                if (errorCharCount[i] <= 1)
+                {
+                    breakOut = false;
+                }
+                ++i;
             }
-        }
-    };
+            if (breakOut)
+            {
+                return;
+            }
 
+            for (int i {}; i < numOfDirections; ++i)
+            {
+                int nextRow = row + directions[i];
+                int nextCol = col + directions[i + 1];
+                if (nextRow >= 0 && nextCol >= 0 && nextRow < matrixSize && nextCol < matrixSize)
+                {
+                    int index = matrixSize * nextRow + nextCol;
+                    searchMatrix(index, nextRow, nextCol, level + 1, errorCharCount);
+                }
+            }
+        };
+
+    // Search through all points in the matrix
     for (int row {}; row < matrixSize; ++row)
     {
         for (int col {}; col < matrixSize; ++col)
         {
             int index = matrixSize * row + col;
-            std::cout << row << col << index << '\n';
-            searchMatrix(index, row, col, 1);
+            std::array<int, numStates> errorCharCount {};
+            searchMatrix(index, row, col, 0, errorCharCount);
         }
     }
     return totalScore;
@@ -81,28 +108,45 @@ int main()
     std::string resultsMatrixStr = loadJSON();
     int const matrixSize         = std::sqrt(resultsMatrixStr.length());
     assert(resultsMatrixStr.length() == matrixSize * matrixSize);
-    setStatesHashMap();
 
     int totalScore = calculateScore(matrixSize, resultsMatrixStr);
 
     if (totalScore >= minThreshold)
     {
         std::cout << "Congratulations! Your Score of " << totalScore << " Passed!\n";
+        // Extra Achievements
         if (totalScore >= 200'000'000)
         {
             std::cout << "Awarded 200 Million Achievement!";
         }
-        if (statesPopulationHashMap["california"])
+        if (! statesVisited.contains("california"))
         {
             std::cout << "Awarded 'No California' Achievement!";
         }
-        if (! statesPopulationHashMap["pennsylvania"])
+        if (statesVisited.contains("pennsylvania"))
         {
             std::cout << "Awarded 'Visited Pennsylvania' Achievement!";
         }
-        // - 20S: visits at least 20 states
-        // - M8: contains all 8 states that begin with an M
-        // - 4C: contains the “Four Corners” states
+        if (statesVisited.size() >= 20)
+        {
+            std::cout << "Awarded 'Visited 20 States' Achievement!";
+        }
+        if (statesVisited.contains("colorado") && statesVisited.contains("utah") && statesVisited.contains("arizona") && statesVisited.contains("newmexico"))
+        {
+            std::cout << "Awarded 'Visited All 4 Corner States' Achievement!"; 
+        }
+        int mCount {};
+        for (const auto& state : statesVisited)
+        {
+            if (state[0] == 'm')
+            {
+                ++mCount;
+            }
+        }
+        if (mCount == 8)
+        {
+            std::cout << "Awarded 'Visited All M States' Achievement!";
+        }
     }
     else
     {
