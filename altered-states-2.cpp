f2133ae39ffc67192ff3e76870e939b6650b92f7
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-28 10:26:49 -0400

    Structured Solution

--- altered-states-2.cpp
+++ altered-states-2.cpp
@@ -1,9 +1,13 @@
 // Copyright 2024 Andrew Drogalis
 // GNU License
+#include <algorithm>
 #include <array>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <string>
+#include <string_view>
+#include <vector>
 
 #include "state-populations.h"
 // inline constexpr int numStates defined in "state-populations"
@@ -13,45 +17,85 @@
 constexpr int matrixSize {5};
 constexpr int numChars {26};
 
-void getConnectionScores()
+struct Solution
 {
-    std::array<std::pair<std::string, double>, numStates> stateAlteredScore {};
-    std::array<std::array<double, numChars>, numChars> charAdjacencyScore {};
-    std::array<std::array<int, numChars>, numChars> charAdjacencyCount {};
-    int i {};
-
-    for (auto [state, population] : statesPopulation)
-    {
-        stateAlteredScore[i] = std::make_pair(state, 0.0);
-        double populationPerChar = population / static_cast<double>(state.length());
-        char lastChar            = ' ';
-        for (char ch : state)
-        {
-            int index = ch - 'a';
-            if (lastChar != ' ')
-            {
-                charAdjacencyCount[lastChar - 'a'][index]++;
-                charAdjacencyScore[lastChar - 'a'][index] += populationPerChar;
-            }
-            lastChar = ch;
-        }
-        ++i;
-    }
-
-    std::array<int, numChars> numOfConnections {};
+    std::array<std::array<char, matrixSize>, matrixSize> resultMatrix {};
+    std::array<int, numChars> numOfUniqueConnections {};
     std::array<double, numChars> connectionsScore {};
-    for (int i {}; i < numChars; ++i)
+    std::array<int, numChars> totalCount {};
+
+    Solution()
     {
-        for (int j {}; j < numChars; ++j)
+        getConnectionScores();
+        findBestFitStates();
+    }
+
+    void getConnectionScores()
+    {
+
+        std::array<std::array<double, numChars>, numChars> charAdjacencyScore {};
+        std::array<std::array<int, numChars>, numChars> charAdjacencyCount {};
+        for (auto const& [state, population] : statesPopulation)
         {
-            if (charAdjacencyCount[i][j])
+            double populationPerChar = population / static_cast<double>(state.length());
+            char lastChar            = ' ';
+            for (char ch : state)
             {
-                numOfConnections[i]++;
-                connectionsScore[i] += charAdjacencyScore[i][j];
+                int index = ch - 'a';
+                if (lastChar != ' ')
+                {
+                    charAdjacencyCount[lastChar - 'a'][index]++;
+                    charAdjacencyScore[lastChar - 'a'][index] += populationPerChar;
+                }
+                lastChar = ch;
+            }
+        }
+
+        for (int i {}; i < numChars; ++i)
+        {
+            for (int j {}; j < numChars; ++j)
+            {
+                if (charAdjacencyCount[i][j])
+                {
+                    numOfUniqueConnections[i]++;
+                    connectionsScore[i] += charAdjacencyScore[i][j];
+                    totalCount[i] += charAdjacencyCount[i][j];
+                }
             }
         }
     }
-}
+
+    void findBestFitStates()
+    {
+        auto comp = [](std::pair<std::string_view, double> A, std::pair<std::string_view, double> B) {
+            return A.second < B.second;
+        };
+        std::vector<std::pair<std::string_view, double>> stateAlteredScore;
+
+        for (auto const& [state, population] : statesPopulation)
+        {
+            double alteredStateZScore {};
+            double minValue = std::numeric_limits<double>::max();
+            for (char ch : state)
+            {
+                alteredStateZScore += connectionsScore[ch - 'a'];
+                minValue = std::min(minValue, connectionsScore[ch = 'a']);
+            }
+            alteredStateZScore -= minValue;
+            double wordMeanZScore = alteredStateZScore / static_cast<double>(state.length() - 1);
+
+            stateAlteredScore.emplace_back(state, wordMeanZScore);
+        }
+        std::sort(stateAlteredScore.rbegin(), stateAlteredScore.rend(), comp);
+
+        for (auto [state, zscore] : stateAlteredScore) { std::cout << state << " " << zscore << '\n'; }
+    }
+
+    [[nodiscard]] std::array<std::array<char, matrixSize>, matrixSize> returnResult() const noexcept
+    {
+        return resultMatrix;
+    }
+};
 
 int saveToJSON(std::string const& resultsMatrixStr)
 {
@@ -76,8 +120,8 @@ int saveToJSON(std::string const& resultsMatrixStr)
 
 int main()
 {
-    getConnectionScores();
-    std::array<std::array<char, matrixSize>, matrixSize> resultsMatrix = {{'a'}};
+    Solution solution;
+    std::array<std::array<char, matrixSize>, matrixSize> resultsMatrix = solution.returnResult();
 
     std::string resultsMatrixStr;
     for (auto row : resultsMatrix)
